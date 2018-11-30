1c062de1bf34b8500d640689d5c100bbd4d2742d
p_jianjyang <p_jianjyang@tencent.com>
2018-11-30 17:17:55 +0800

    00000000

---
+++ _rooter.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'isActive' : true,
+    'local' : {
+        'activity.yqacg.com/2018/00000000': {
+            'views': '/2018/00000000/index', 
+            'cgi': '/page/2018/00000000/index' // 直出数据的接口
+        }
+    },
+    'dynamic' : {
+        'activity.yqacg.com/2018/00000000': {
+            'views': '/2018/00000000/index', 
+            'cgi': '' // 直出数据的接口
+        }
+    }
+};

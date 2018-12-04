d78a7e200084a48655affc87c452671df5037542
yangjian <yangj0412@foxmail.com>
2018-12-04 21:55:03 +0800

    Update _rooter.js

--- _rooter.js
+++ _rooter.js
@@ -1,15 +1,9 @@
 module.exports = {
-    'isActive' : true,
     'local' : {
-        'activity.yqacg.com/2018/00000000': {
-            'views': '/2018/00000000/index', 
-            'cgi': '/page/2018/00000000/index' // 直出数据的接口
-        }
-    },
-    'dynamic' : {
-        'activity.yqacg.com/2018/00000000': {
-            'views': '/2018/00000000/index', 
-            'cgi': '' // 直出数据的接口
+        'test/2018/00000000': {
+            'test': '/2018/00000000/index', 
+            'test': '/page/2018/00000000/index' // 直出数据的接口
         }
     }
+  
 };
